5acf1a37892db5e25fca57096b76f4f2b65dd924
JoaoLamounier <joaopaulolamou@gmail.com>
2021-06-12 16:22:25 -0300

    alterado fundo, adicionado funcao

---
+++ tv.js
@@ -0,0 +1,9 @@
+function menubar() {
+    var imgatual= document.getElementById("imgtv").style.background //aqui busca em qual estado está//
+    var estadotela
+};
+
+function mudarimg(){
+    var imgatual= document.getElementById("imgtv").style.background
+
+}
