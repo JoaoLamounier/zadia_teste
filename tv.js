366619f0d0ded151adc57bf615a1f209629ff7c3
JoaoLamounier <joaopaulolamou@gmail.com>
2021-06-13 19:03:25 -0300

    função mudar estado funcional

--- tv.js
+++ tv.js
@@ -1,9 +1,10 @@
 function initmenu() {
     //aqui busca em qual estado está//
-    estadotela = document.querySelector("id", "tv")
+    estadotela = document.getElementById("tv")
     var c = document.getElementById("tv").childNodes
     //aqui são colocados id's nas divs inferiores para começar a mostrar comandos e adicionar onclicks nos botões//
-    c[1].setAttribute("id","tv") //não faço ideia do porque mas esta apenas salvando nos numeros impares da array
+    //não faço ideia do porque mas esta apenas salvando nos numeros impares da array
+    c[1].setAttribute("id","tvzina") 
     c[3].setAttribute("id","lojadescricao")
     c[5].setAttribute("id","qrcode")
     c[7].setAttribute("id","loja")
@@ -11,7 +12,7 @@ function initmenu() {
     c[9].addEventListener("click", mudarimg, false)
     c[11].setAttribute("id","botaodentro");
     c[11].addEventListener("click", mudarimg, false)
-    c[13].setAttribute("id","botaotras");
+    c[13].setAttribute("id","botaoatras");
     c[13].addEventListener("click", mudarimg, false)
     estadotela.setAttribute("id","tvmenu"); //aqui é alterado o estado atual da tela//
 };
